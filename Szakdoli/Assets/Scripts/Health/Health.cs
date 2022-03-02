d6fdcdc4ae22833f7b0b5844c7b6e3a577af580d
abuska <abigelcsabai@gmail.com>
2022-03-02 18:22:50 +0100

    iFrames

--- Szakdoli/Assets/Scripts/Health/Health.cs
+++ Szakdoli/Assets/Scripts/Health/Health.cs
@@ -1,20 +1,29 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour{
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth{get; private set; }
     private Animator anim;
     private bool dead;
-    
+
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private float numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake(){
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _demage){
         currentHealth = Mathf.Clamp(currentHealth - _demage, 0, startingHealth);
         if(currentHealth>0){
             anim.SetTrigger("hurt");
+            StartCoroutine(Invonerability());
         }else{
             if(!dead){
                 anim.SetTrigger("die");
@@ -24,7 +33,21 @@ public class Health : MonoBehaviour{
           
         }
     }
+
     public void AddHelath(float _value){
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
+
+    private IEnumerator Invonerability(){
+        Physics2D.IgnoreLayerCollision(10,11,true);
+        for(int i=0;i<numberOfFlashes; i++){
+            spriteRend.color = new Color(255,0,0,0.5f);
+            yield return new WaitForSeconds(iFramesDuration/(numberOfFlashes*2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration/(numberOfFlashes*2));
+        }
+         Physics2D.IgnoreLayerCollision(10,11,false);
+    }
+
+
 }
