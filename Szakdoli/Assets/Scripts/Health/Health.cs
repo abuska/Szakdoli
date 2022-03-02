f6d51eda11bc0a0214f9e96b31bd11cc5d983f03
abuska <abigelcsabai@gmail.com>
2022-03-02 16:48:26 +0100

    Hurt & Die animation, create saw

--- Szakdoli/Assets/Scripts/Health/Health.cs
+++ Szakdoli/Assets/Scripts/Health/Health.cs
@@ -3,23 +3,25 @@ using UnityEngine;
 public class Health : MonoBehaviour{
     [SerializeField] private float startingHealth;
     public float currentHealth{get; private set; }
+    private Animator anim;
+    private bool dead;
     
     private void Awake(){
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDemage(float _demage){
         currentHealth = Mathf.Clamp(currentHealth - _demage, 0, startingHealth);
         if(currentHealth>0){
-
+            anim.SetTrigger("hurt");
         }else{
-
+            if(!dead){
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+          
         }
     }
-    private void Update(){
-        if(Input.GetKeyDown(KeyCode.E)){
-            TakeDemage(1);
-        }
-    }
-
 }
