454114f727fbf147353117a4b0a2c54333e2cf92
abuska <abigelcsabai@gmail.com>
2022-03-22 16:34:05 +0100

    ElevatorFix

--- Szakdoli/Assets/Scripts/Rooms/AntigravityElevator.cs
+++ Szakdoli/Assets/Scripts/Rooms/AntigravityElevator.cs
@@ -16,7 +16,8 @@ public class AntigravityElevator : MonoBehaviour{
     private void OnTriggerExit2D(Collider2D collision){
         if(collision.tag == "Player"){
             collision.GetComponent<Rigidbody2D>().velocity = new Vector2(collision.GetComponent<Rigidbody2D>().velocity.x, 0);
-            collision.GetComponent<PlayerMovement>().setGravityScale(3);
+            collision.GetComponent<PlayerMovement>().setGravityScale(gravityScale);
+            gravityScale = 0;
         }
     }
 
