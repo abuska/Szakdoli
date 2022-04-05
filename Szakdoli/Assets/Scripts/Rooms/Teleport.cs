2fe6d50ba9e571772e6e6a1a6a578a0cda44fd8e
abuska <abigelcsabai@gmail.com>
2022-04-05 19:25:34 +0200

    teleport

---
+++ Szakdoli/Assets/Scripts/Rooms/Teleport.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour{
+    
+    [SerializeField] private Transform otherSide;
+    private float teleportTimer = Mathf.Infinity;
+
+   
+    private void OnTriggerStay2D(Collider2D collision){
+        if(collision.tag == "Player" && Input.GetKey(KeyCode.E) && teleportTimer>0.5f){
+            collision.transform.position = otherSide.transform.position;
+            teleportTimer=0;
+        }
+        teleportTimer+=Time.deltaTime;
+    }
+}
