816fa9b7cac9b1eeb88e902fd526cf6ae4a324d8
abuska <abigelcsabai@gmail.com>
2022-04-11 12:23:05 +0200

    shield jump & lvl fix

--- Szakdoli/Assets/Scripts/Player/Shield.cs
+++ Szakdoli/Assets/Scripts/Player/Shield.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class Shield : MonoBehaviour
 {
     private Animator anim;
+  
+    [SerializeField] private BoxCollider2D boxCollider;
+
     private bool isShieldUp;
 
     void Start(){
-         anim = GetComponentInParent<Animator>();
+        anim = GetComponentInParent<Animator>();
 
     }
     void Update()
     {
         if(anim.GetBool("isShieldUp") && !isShieldUp){
+          
+            
             isShieldUp = true;
+            boxCollider.usedByEffector = true;
             Physics2D.IgnoreLayerCollision( 10, 17, false);
         }else if(!anim.GetBool("isShieldUp") && isShieldUp){
             isShieldUp = false;
+            boxCollider.usedByEffector = false;
             Physics2D.IgnoreLayerCollision( 10, 17, true);
         }
         
