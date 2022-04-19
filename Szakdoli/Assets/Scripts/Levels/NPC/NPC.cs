ba4601fd61cde5d83475f59d5a488b9b8d560c93
abuska <abigelcsabai@gmail.com>
2022-04-19 18:02:27 +0200

    NPC

---
+++ Szakdoli/Assets/Scripts/Levels/NPC/NPC.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private GameObject infoText;
+    [SerializeField] private bool showInfoInTrigger;
+    private bool isPlayerInCollider;
+    
+
+    private void Update(){
+        if(isPlayerInCollider){
+            if(showInfoInTrigger || Input.GetKey(KeyCode.E)){
+                infoText.SetActive(true);
+            }
+        }else{
+            infoText.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.tag == "Player"){
+           isPlayerInCollider = true;
+        }
+    }
+}
