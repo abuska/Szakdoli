c22eeec76b179533919f6687fdec27468a92e901
abuska <abigelcsabai@gmail.com>
2022-05-11 17:12:17 +0200

    prefab fix

--- Szakdoli/Assets/Scripts/Core/CameraController.cs
+++ Szakdoli/Assets/Scripts/Core/CameraController.cs
@@ -13,15 +13,29 @@ public class CameraController : MonoBehaviour{
     private PlayerManager playerManager;
     private float lookAhead;
 
+    private float cameraLimitUp;
+    private float cameraLimitDown;
+    private float cameraLimitLeft;
+    private float cameraLimitRight;
+
     private void Awake(){
         playerManager = FindObjectOfType<PlayerManager>();
+        
+        cameraLimitUp = this.transform.GetChild(0).gameObject.GetComponent<Transform>().position.y;
+        cameraLimitDown = this.transform.GetChild(1).gameObject.GetComponent<Transform>().position.y;
+        cameraLimitLeft = this.transform.GetChild(2).gameObject.GetComponent<Transform>().position.x;
+        cameraLimitRight = this.transform.GetChild(3).gameObject.GetComponent<Transform>().position.x;
     }
 
     public void FollowPlayerCamera(){
 
         player = playerManager.getActivePlayerTransform();
 
-        transform.position = new Vector3(player.position.x + lookAhead, player.position.y, transform.position.z);
+        transform.position = new Vector3(
+            Mathf.Clamp(player.position.x + lookAhead, cameraLimitLeft, cameraLimitRight), 
+            Mathf.Clamp(player.position.y, cameraLimitDown, cameraLimitUp), 
+            transform.position.z
+        );
 
         lookAhead = Mathf.Lerp(lookAhead, (aheadDistance*player.localScale.x), Time.deltaTime * cameraSpeed );
 
